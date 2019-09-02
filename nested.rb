495cc176c709ce7aba0291f47f25ad7ad2385d57
Yasmine Hajaji <yasminehajaji95@gmail.com>
2019-09-02 06:53:29 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,28 +82,12 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash = 
-{
-  :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        },
-        :yukihiro_matsumoto => {
-          :known_for => "Ruby",
-          :languages => ["LISP", "C"]
-        }
-}
+ programmer_hash[:yukihiro_matsumoto] = {}
+		 programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+		 programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
+		 programmer_hash
 
 
-puts programmer_hash
     
 end
 
